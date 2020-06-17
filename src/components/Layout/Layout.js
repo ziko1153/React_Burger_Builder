6c82465dfaece789e0df42bc9d18fe445d8c1c88
Tahmid Ziko <tahmidziko@gmail.com>
2020-06-17 14:32:44 +0600

    First Commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Layout = props => (
+  <>
+    <div>Toolbar,scrollbar.backdrop</div>
+    {props.children}
+  </>
+);
+
+export default Layout;
---
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <>
+        <div>Burger</div>
+        <div>Burger Control Option</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
